c514585b0f5fa962009ea6eafade442faacbea02
James Barber <james@jamesbarber.tech>
2022-07-05 17:55:18 +0100

    Tests

--- app/auth.php
+++ app/auth.php
@@ -44,7 +44,7 @@ if (!isset($_GET['code'])) {
     try {
 
         // Try to get an access token using the authorization code grant.
-        $accessToken = $provider->getAccessToken('authorization_code', [
+        $accessToken = $provider->getAccessToken('member:read', [
             'code' => $_GET['code']
         ]);
 
