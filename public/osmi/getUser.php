b063bb293b5e65fd17b5c2dfdd2cdf570fc936e9
James Barber <james@jamesbarber.tech>
2022-07-07 17:35:11 +0100

    Another bug fix

--- public/osmi/getUser.php
+++ public/osmi/getUser.php
@@ -5,7 +5,7 @@ $token = $_SERVER['HTTP_AUTHORIZATION'];
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://www.onlinescoutmanager.co.uk/oauth/resource");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, "Authorization: " . $token);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: " . $token));
 $res = curl_exec($ch);
 curl_close($ch);
 
