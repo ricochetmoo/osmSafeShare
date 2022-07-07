cba1d658c897a6c3def354456aefd4c2e5883e20
James Barber <james@jamesbarber.tech>
2022-07-07 16:40:05 +0100

    JS bug

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 function redirectIfLoggedOut()
 {
-	if (!Cookies.get('oauth-token'))
+	if (Cookies.get('oauth-token') != undefined)
 	{
 		window.location.href = 'login.html';
 	}
--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 function redirectIfLoggedIn()
 {
-	if (Cookies.get('oauth-token'))
+	if (Cookies.get('oauth-token') == undefined)
 	{
 		window.location.href = 'index.html';
 	}
